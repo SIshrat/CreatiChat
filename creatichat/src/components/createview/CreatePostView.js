4934f963df7b0257ef25f96835f904049a4c1fcf
co12468 <co12468@uga.edu>
2023-11-20 21:19:08 -0500

    Added to create post view

--- creatichat/src/components/createview/CreatePostView.js
+++ creatichat/src/components/createview/CreatePostView.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from '../common/header/Header';
+import Navbar from '../common/navbar/Navbar';
+import CreateBox from '../common/postbox/CreateBox';
 
 const CreatePostView = (props) => {
-    
+    return(
+        <>
+            <Header />
+            <Navbar />
+            <CreateBox />
+        </>
+    );
 }
 
 export default CreatePostView;
