428beeb683d20c8ab0e464ba28da100ced4b2997
co12468 <co12468@uga.edu>
2023-11-20 23:43:34 -0500

    Added content.css to format post view better

--- src/components/common/post/Content.js
+++ src/components/common/post/Content.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Content.css';
 
 const Content = props => {
     return(
-        <li key={props.postId} className="post-info">
+        <div key={props.postId} className="post-info">
             <h5>{props.title}</h5>
             <p>{props.description}</p>
             <h6>{props.date}</h6>
-        </li>
+        </div>
     );
 }
 
