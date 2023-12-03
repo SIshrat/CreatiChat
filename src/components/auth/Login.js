0897c3b91446d3e53225392fcb49c6421b2771d0
co12468 <co12468@uga.edu>
2023-12-02 19:04:52 -0500

    Added login and sign up css

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import Button from "../common/Button";
 import axios from "axios";
+import "./Login.css"
 
 const Login = (props) => {
     const [enteredUsername,setEnteredUsername] = useState('');
     const [enteredPassword, setEnteredPassword] = useState('');
+    const [displayPassword, setDisplayPassword] = useState('');
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
     }
     const passwordChangeHandler = (event) => {
-        setEnteredPassword(event.target.value);
+        const value = event.target.value;
+        setEnteredPassword(value);
+        setDisplayPassword('●'.repeat(value.length));
     }
     
 
@@ -26,23 +30,29 @@ const Login = (props) => {
     return (
         <div className="auth-view">
             <h2>Login</h2>
-            <form  onSubmit={submitHandler}>
-                <label>Username:</label>
-                <input 
-                    type="text"
-                    id="username"
-                    value={enteredUsername}
-                    onChange={usernameChangeHandler}
-                />
-                <label>Password:</label>
-                <input
-                    type="text"
-                    id="password"
-                    value={enteredPassword}
-                    onChange={passwordChangeHandler}
-                />
-                <Button type="submit">Login</Button>
-            </form>            
+            <div className="login-container">
+                <form  onSubmit={submitHandler} className="login-form">
+                    <div className="login-input">
+                        <label>Username:</label>
+                        <input 
+                            type="text"
+                            id="username"
+                            value={enteredUsername}
+                            onChange={usernameChangeHandler}
+                        />
+                    </div>
+                    <div className="login-input">
+                        <label>Password:</label>
+                        <input
+                            type="text"
+                            id="password"
+                            value={displayPassword}
+                            onChange={passwordChangeHandler}
+                        />
+                    </div>
+                    <Button type="submit">Login</Button>
+                </form>
+            </div>            
         </div>
     );
 }
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import defaulAvatar from '../../images/defaultAvatar.jpg';
 import Button from "../common/Button";
+import "./Login.css"
 
 const Signup = (props) => {
     const [enteredUsername,setEnteredUsername] = useState('');
     const [enteredPassword, setEnteredPassword] = useState('');
+    const [displayPassword, setDisplayPassword] = useState('');
     const [enteredAvatar, setEnteredAvatar] = useState('');
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
     }
     const passwordChangeHandler = (event) => {
-        setEnteredPassword(event.target.value);
+        const value = event.target.value;
+        setEnteredPassword(value);
+        setDisplayPassword('●'.repeat(value.length));
     }
     const avatarChangeHandler = (event) => {
         setEnteredAvatar(event.target.value);
@@ -42,30 +46,38 @@ const Signup = (props) => {
     return (
         <div className="auth-view">
             <h2>Signup</h2>
-            <form onSubmit={submitHandler}>
-                <label>Username:</label>
-                <input 
-                    type="text"
-                    id="username"
-                    value={enteredUsername}
-                    onChange={usernameChangeHandler}
-                />
-                <label>Password:</label>
-                <input
-                    type="text"                    
-                    id="password"
-                    value={enteredPassword}
-                    onChange={passwordChangeHandler}
-                />
-                <label>Image URL:</label>
-                <input
-                    type="text"
-                    id="avatar"
-                    value={enteredAvatar}
-                    onChange={avatarChangeHandler}
-                />
-                <Button type="submit">Signup</Button>
-            </form>            
+            <div className="login-container">
+                <form onSubmit={submitHandler} className="login-form">
+                    <div className="login-input">
+                        <label>Username:</label>
+                        <input 
+                            type="text"
+                            id="username"
+                            value={enteredUsername}
+                            onChange={usernameChangeHandler}
+                        />
+                    </div>
+                    <div className="login-input">
+                        <label>Password:</label>
+                        <input
+                            type="text"                    
+                            id="password"
+                            value={displayPassword}
+                            onChange={passwordChangeHandler}
+                        />
+                    </div>
+                    <div className="login-input">
+                        <label>Image URL:</label>
+                        <input
+                            type="text"
+                            id="avatar"
+                            value={enteredAvatar}
+                            onChange={avatarChangeHandler}
+                        />
+                    </div>
+                    <Button type="submit">Signup</Button>
+                </form>       
+            </div>     
         </div>
     );
 }
